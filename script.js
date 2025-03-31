eb1ff7d089ea19f433d8f4241e6fd410f3f24559
ingrid2601 <159648748+ingrid2601@users.noreply.github.com>
2025-03-31 00:48:10 +0000

    s

--- script.js
+++ script.js
@@ -4,17 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = ${tamanhoAtualFonte}rem
-    })
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        })
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1
-    })
-
-
-
-
-
-
-
-
+        diminuiFonteBotaoFonteBotao.addEventListener('click', function () {
+            tamanhoAtualFonte -= 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        })
+})
